f22eba3178c400269bfc388c408b85fe77f047ce
SvishD <d.svishchuk@gmail.com>
2016-10-09 15:03:21 +0300

    adaptive

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,25 @@
 (function() {
   'use strict';
+  $('.auth-btn').click(function(event) {
+    event.preventDefault();
+    $('.card__content').css("transform","rotateY(180deg)");
+    $(this).css("display","none");
+    setTimeout( function(){
+    $('.card__content').css("display","none"); 
+    $('.auth-form').css("display","block");
+    },1000);
+    })
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+  $('.index-btn').click(function(event) {
+    event.preventDefault();
+    $('.auth-form').css("transform","rotateY(180deg)");
+    $('.card__content').css("transform","rotateY(360deg)");
+    setTimeout( function(){
+    $('.auth-form').css("display","none"); 
+    $('.card__content').css("display","block");
+    $('.auth-btn').css("display","inline-block");
+    $('.auth-form').css("transform","rotateY(360deg)");
+    },1000);
+    })
+ 
 })();
