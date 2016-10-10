026be6a1bb0241078eca448d75b6afc8adf2e1ad
SvishD <d.svishchuk@gmail.com>
2016-10-10 10:01:32 +0300

    google map

--- source/js/_map.js
+++ source/js/_map.js
@@ -1,4 +1,3 @@
-
 var map = (function () {
 
 function init() {
@@ -24,31 +23,4 @@ function init() {
     init: init
   };
 
-})();
-
-
-(function() {
-  'use strict';
-  $('.auth-btn').click(function(event) {
-    event.preventDefault();
-    $('.card__content').css("transform","rotateY(180deg)");
-    $(this).css("display","none");
-    setTimeout( function(){
-    $('.card__content').css("display","none"); 
-    $('.auth-form').css("display","block");
-    },1000);
-    })
-
-  $('.index-btn').click(function(event) {
-    event.preventDefault();
-    $('.auth-form').css("transform","rotateY(180deg)");
-    $('.card__content').css("transform","rotateY(360deg)");
-    setTimeout( function(){
-    $('.auth-form').css("display","none"); 
-    $('.card__content').css("display","block");
-    $('.auth-btn').css("display","inline-block");
-    $('.auth-form').css("transform","rotateY(360deg)");
-    },1000);
-    })
- 
 })();
--- source/js/map.js
+++
@@ -1,7 +0,0 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 59.94029288, lng: 30.31376147},
-    zoom: 8
-  });
-}
